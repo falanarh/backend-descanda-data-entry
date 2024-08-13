116fe838752893e671a9280d1d05a16686bc3f9f
222112038 <falanarofako29@gmail.com>
2024-08-13 21:47:36 +0700

    change is_umkm property type

--- src/models/listingUmkmModel.ts
+++ src/models/listingUmkmModel.ts
@@ -1,11 +1,11 @@
-import mongoose, { Document, Schema } from 'mongoose';
+import mongoose, { Document, Schema } from "mongoose";
 
 interface IListingUmkm extends Document {
   rt_rw_dusun: string;
   no_urut_bangunan: string;
   nama_kk_ak_bangunan: string;
   alamat: string;
-  is_umkm: boolean;
+  is_umkm: "ya" | "tidak";
   email_pcl: string;
   kode: string; // Add kode property
 }
@@ -15,10 +15,10 @@ const ListingUmkmSchema: Schema = new Schema({
   no_urut_bangunan: { type: String, required: true },
   nama_kk_ak_bangunan: { type: String, required: true },
   alamat: { type: String, required: true },
-  is_umkm: { type: Boolean, default: false },
+  is_umkm: { type: String, enum: ["ya", "tidak"], required: true },
   email_pcl: { type: String, required: true },
   kode: { type: String, required: true, unique: true }, // Add kode field
 });
 
 export { IListingUmkm };
-export default mongoose.model<IListingUmkm>('ListingUmkm', ListingUmkmSchema);
+export default mongoose.model<IListingUmkm>("ListingUmkm", ListingUmkmSchema);
