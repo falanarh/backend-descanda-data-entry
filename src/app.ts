f153cf4b616295a73338e23811a7144fafdd3d8d
222112038 <falanarofako29@gmail.com>
2024-08-14 08:00:27 +0700

    add cors

--- src/app.ts
+++ src/app.ts
@@ -16,19 +16,19 @@ connectDB();
 
 app.use(morgan("dev"));
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"], // Metode yang diizinkan
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST", "PUT", "DELETE"], // Metode yang diizinkan
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 
-// app.use(cors({
-//   origin: ['https://desa-cantik-sda.vercel.app', 'http://localhost:5173'], // Ganti dengan domain Anda
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Metode yang diizinkan
-//   // allowedHeaders: ['Content-Type', 'Authorization']
-// }));
+app.use(cors({
+  origin: ['https://descanda-data-entry.vercel.app', 'http://localhost:5173'], // Ganti dengan domain Anda
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Metode yang diizinkan
+  // allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 // app.use(bodyParser.json());
